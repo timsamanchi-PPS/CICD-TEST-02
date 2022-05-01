37f18ee997dd83c579f63cb50c743de994a0b3eb
tim samanchi <tim.samanchi@pps.edenred.com>
2022-05-01 14:10:54 +0100

    initual push
    initial push

--- cicd.tf
+++ cicd.tf
@@ -27,3 +27,94 @@ resource "aws_codebuild_project" "tf-plan" {
       buildspec = file("buildspec/plan-buildspec.yml")
     }
 }
+
+# 1.b- terraform apply
+resource "aws_codebuild_project" "tf-apply" {
+    description = "apply stage for terraform"
+    name = "tf-cicd-apply"
+    service_role = aws_iam_role.tf-codebuild-role.arn
+
+    artifacts {
+      type = "CODEPIPELINE"
+    }
+
+    environment {
+      compute_type = "BUILD_GENERAL1_SMALL"
+      image = "hashicorp/terraform:latest"
+      type = "LINUX_CONTAINER"
+      image_pull_credentials_type = "SERVICE_ROLE"
+      registry_credential {
+        credential = var.dockerHub-credentials
+        credential_provider = "SECRETS_MANAGER"
+      }
+    }
+    source {
+      type = "CODEPIPELINE"
+      buildspec = file("buildspec/apply-buildspec.yml")
+    }
+}
+
+# 2- aws-codepipeline
+resource "aws_codepipeline" "cicd-pipeline" {
+    name = "tf-cicd"
+    role_arn = aws_iam_role.tf-codepipeline-role.arn
+
+    artifact_store {
+      type = "S3"
+      location = aws_s3_bucket.artifacts.id
+    }
+
+    # Source Stage
+    stage {
+      name = "Source"
+      action {
+          name = "Source"
+          version = "1"
+          category = "Source"
+          owner = "AWS"
+          output_artifacts = ["tf-code"]
+
+          provider = "CodeStarSourceConnection"
+          configuration = {
+            FullRepositoryId = "timsamanchi-PPS/CICD-TEST-02"
+            BranchName = "master"
+            ConnectionArn = var.codestar-connector 
+            OutputArtifactFormat = "CODE_ZIP"
+          }
+      }
+    }
+
+    # Stage Plan
+    stage {
+        name = "Plan"
+        action {
+            name = "Build"
+            version = "1"
+            category = "Build"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+
+            provider = "CodeBuild"
+            configuration = {
+              ProjectName = "tf-cicd-plan"
+            }
+        }
+    }
+
+    # Stage Apply
+    stage {
+        name = "Apply"
+        action {
+            name = "Build"
+            version = "1"
+            category = "Build"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+
+            provider = "CodeBuild"
+            configuration = {
+              ProjectName = "tf-cicd-apply"
+            }
+        }
+    }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -2,3 +2,6 @@ aws-region = "eu-west-2"
 artifacts-bucket = "tf-artifacts-test"
 permissions_boundary = "arn:aws:iam::714102873737:policy/pps.global.iamrp.ConanPermissionBoundaryPolicy"
 dockerHub-credentials = "arn:aws:secretsmanager:eu-west-2:714102873737:secret:dockerHub-credentials-5VJlg9"
+codestar-connector = "arn:aws:codestar-connections:eu-west-2:714102873737:connection/b3ddb887-8b2c-431e-9e92-6658a0bd1650"
+
+
